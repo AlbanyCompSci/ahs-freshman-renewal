031dd9059269089c0159299e99171a9b9add8546
David Harrison <djrh777@gmail.com>
2015-03-31 16:44:15 -0700

    Remove Browserify from Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,5 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        react: {
-            options: {
-                harmony: true
-            },
-            dynamic_mappings: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'src',
-                        src: ['**/*.jsx'],
-                        dest: 'dist',
-                        ext: '.js'
-                    }
-                ]
-            }
-        },
-        browserify: {
-            browserifyOptions: {
-                transform: [ require('grunt-react').browserify ]
-            },
-            app: {
-                src: 'dist/index.js',
-                dest: 'index.js'
-            }
-        },
+    grunt.initConfig({ pkg: grunt.file.readJSON('package.json'),
         jshint: {
             files: ['gruntfile.js', 'src/**/*.jsx', 'test/**/*.js'],
             options: {
@@ -34,8 +8,5 @@ module.exports = function(grunt) {
         },
     });
     grunt.loadNpmTasks('grunt-jsxhint');
-    grunt.loadNpmTasks('grunt-react');
-    grunt.loadNpmTasks('grunt-browserify');
     grunt.registerTask('lint', ['jshint']);
-    grunt.registerTask('default', ['react', 'browserify']);
 }
