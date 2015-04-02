c6d659445e9c0a5ab0ad593821cbbfdb1dfe5b80
David Harrison <djrh777@gmail.com>
2015-04-01 18:55:43 -0700

    Added Webpack tasks to Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,21 @@
+var _ = require('lodash');
+
 module.exports = function(grunt) {
+    var webpackBasic = {
+        entry: './src/index.jsx',
+        output: {
+            filename: './index.js'
+        },
+        module: {
+            loaders: [
+                { test: /\.jsx$/, loader: 'jsx-loader?harmony' },
+                { test: /\.css$/, loader: 'style-loader!css-loader' }
+            ]
+        },
+        resolve: {
+            extensions: ['', '.js', '.jsx']
+        }
+    }
     grunt.initConfig({ pkg: grunt.file.readJSON('package.json'),
         jshint: {
             files: ['gruntfile.js', 'src/**/*.jsx', 'test/**/*.js'],
@@ -6,7 +23,23 @@ module.exports = function(grunt) {
                 esnext: true
             }
         },
+        webpack: {
+            dev: webpackBasic,
+            devWatch: _.assign(_.clone(webpackBasic), {watch: true}),
+            production: _.assign(
+                _.clone(webpackBasic),
+                {
+                    optimize: {minimize: true},
+                    output: {filename: 'deploy/index.js'}
+                }
+            )
+        }
     });
     grunt.loadNpmTasks('grunt-jsxhint');
+    grunt.loadNpmTasks('grunt-webpack');
+    grunt.loadNpmTasks('grunt-keepalive');
     grunt.registerTask('lint', ['jshint']);
+    grunt.registerTask('dev-build', ['webpack:dev']);
+    grunt.registerTask('dev-watch', ['webpack:devWatch', 'keepalive']);
+    grunt.registerTask('build', ['webpack:production']);
 }
