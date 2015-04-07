27c761ffc7788e091835e7731c536f0e7dbfbe51
David Harrison <djrh777@gmail.com>
2015-04-07 12:20:33 -0700

    Change name of Firebase root URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var Secret = require('./secret.js');
 
-exports.FIREBASE_REFERENCE = 'https://ahs-freshman-renewal.firebaseio.com';
+exports.FIREBASE_ROOT = 'https://ahs-freshman-renewal.firebaseio.com';
 exports.FIREBASE_TOKEN = Secret.FIREBASE_TOKEN;
 exports.BACKUP_DIR = 'backup';
