78e055fc4fec3b018f5333e470489474c99b845a
David Harrison <djrh777@gmail.com>
2015-04-09 15:39:15 -0700

    Set default title in generated HTML

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,35 +4,37 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var Config = require('./config');
 
+var webpackConfig = function(config) {return {
+    entry: './src/main.jsx',
+    output: {
+        path: __dirname + '/' + (config.dir || '.'),
+        filename: 'index.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.jsx$/, loader: 'jsx?harmony' },
+            { test: /\.less$/, loader: 'style!css!less' },
+            { test: /\.css$/, loader: 'style!css' },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
+        ],
+        // TODO: new version of Parse (expected mid Apr. 2015) should fix
+        // the import for Parse and make this unecessary
+        noParse: /parse-latest.js/
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.css', '.less']
+    },
+    optimize: {
+        minimize: config.minimize || false
+    },
+    watch: config.watch || false,
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Albany High School Freshman Debates',
+        filename: 'index.html'
+    })]
+}}
+
 module.exports = function(grunt) {
-    var webpackConfig = function(config) {return {
-        entry: './src/main.jsx',
-        output: {
-            path: __dirname + '/' + (config.dir || '.'),
-            filename: 'index.js'
-        },
-        module: {
-            loaders: [
-                { test: /\.jsx$/, loader: 'jsx?harmony' },
-                { test: /\.less$/, loader: 'style!css!less' },
-                { test: /\.css$/, loader: 'style!css' },
-                { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
-            ],
-            // TODO: new version of Parse (expected mid Apr. 2015) should fix
-            // the import for Parse and make this unecessary
-            noParse: /parse-latest.js/
-        },
-        resolve: {
-            extensions: ['', '.js', '.jsx', '.css', '.less']
-        },
-        optimize: {
-            minimize: config.minimize || false
-        },
-        watch: config.watch || false,
-        plugins: [new HtmlWebpackPlugin({
-            filename: 'index.html'
-        })]
-    }}
     grunt.initConfig({ pkg: grunt.file.readJSON('package.json'),
         jshint: {
             files: ['gruntfile.js', 'src/**/*.js*', 'test/**/*.js'],
